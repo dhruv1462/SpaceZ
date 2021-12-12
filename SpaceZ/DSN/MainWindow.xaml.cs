367b04ea51e418fccbed29139566c0b45c904f5f
dhruv <dhruvpatel1462@outlook.com>
2021-12-12 11:53:58 -0700

    Basic Form Created

---
+++ SpaceZ/DSN/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DSN
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void communicationSystemBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var communicationSystem = new CommunicationSystem(); //create your new form.
+            communicationSystem.Show(); //show the new form.
+            this.Close();
+        }
+
+        private void missionControlSystemBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var missionControlSystem = new MissionControlSystem(); //create your new form.
+            missionControlSystem.Show(); //show the new form.
+            this.Close();
+        }
+    }
+}
---
+++ SpaceZ/DSN/MissionControlSystem.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DSN
+{
+    /// <summary>
+    /// Interaction logic for MissionControlSystem.xaml
+    /// </summary>
+    public partial class MissionControlSystem : Window
+    {
+        public MissionControlSystem()
+        {
+            InitializeComponent();
+        }
+
+
+        private void addSpaceCraftBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var communicationSystem = new CommunicationSystem(); //create your new form.
+            communicationSystem.Show(); //show the new form.
+            this.Close();
+        }
+    }
+}
