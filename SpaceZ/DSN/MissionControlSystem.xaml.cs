4a060a11d45918792a59604930bbdf09f1b7973f
dhruv <dhruvpatel1462@outlook.com>
2021-12-13 19:45:12 -0700

    Mission Control System setted up.

--- SpaceZ/DSN/MissionControlSystem.xaml.cs
+++ SpaceZ/DSN/MissionControlSystem.xaml.cs
@@ -37,6 +37,16 @@ namespace DSN
             {
                 launchSpacecraftCombo.Items.Add(reader.GetValue(0));
             }
+
+            reader.Close();
+
+            selectQuery = "Select spacecraftName from spacecraftinfo";
+            cmd = new SqlCommand(selectQuery, cnn);
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                listOfSpacecrafts.Items.Add(reader.GetValue(0));
+            }
             cnn.Close();
         }
 
@@ -71,6 +81,33 @@ namespace DSN
         private void comboNewSpacecrafts_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+
+        private void listOfSpacecrafts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string spacecraftName = listOfSpacecrafts.SelectedItem.ToString();
+            SqlConnection cnn;
+            string connetionString;
+            connetionString = @"Server=tcp:spacez.database.windows.net,1433;Initial Catalog=SpaceZ;Persist Security Info=False;User ID=dpatel81;Password=Dilip_1462!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            SqlCommand cmd;
+            SqlDataReader reader;
+            string selectQuery = "Select spacecraftName, launchStatus from spacecraftinfo Where spacecraftName = '" + spacecraftName + "'";
+            cmd = new SqlCommand(selectQuery, cnn);
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                MessageBox.Show(reader.GetString(1), reader.GetString(0));
+            }
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
         }
     }
 }
